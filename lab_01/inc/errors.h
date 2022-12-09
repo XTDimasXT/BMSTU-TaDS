70a83cbbdef59c06eacac94b586bb51f8e5e4eec
Pisarenko Dmitriy <pdp21u515@student.bmstu.ru>
2022-12-09 20:48:55 +0100

    lab 01 06 updates

--- lab_01/inc/errors.h
+++ lab_01/inc/errors.h
@@ -1,8 +1,14 @@
 #ifndef __ERRORS_H__
 #define __ERRORS_H__
 
-#define NUM_READ_ERROR 1
-#define ZERO_DIVISION_ERROR 2
-#define EXPONENT_ERROR 3
+#define EMPTY_INPUT_ERROR 1
+#define SIGN_ERROR 2
+#define OVERFLOW_ERROR 3
+#define INPUT_DOT_ERROR 4
+#define INPUT_DIGIT_ERROR 5
+#define TYPE_ERROR 6
+#define ORDER_ERROR 7
+#define DIVISION_BY_ZERO 8
+#define ZERO_ERROR 9
 
 #endif // __ERRORS_H__
--- lab_01/src/input.c
+++ lab_01/src/input.c
@@ -1,136 +1,160 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-#include "input.h"
+#include "../inc/structs.h"
+#include "../inc/input.h"
+#include "../inc/constants.h"
+#include "../inc/errors.h"
 
-int big_int_input(big_int_t *num)
+int my_float_input(float_t *a)
 {
-    num->is_negative = 0;
-    size_t len = 0;
-    char current = getc(stdin);
-
-    if (current == '-')
-        num->is_negative = 1;
-    else if (current == '+')
-        num->is_negative = 0;
-    else if (isdigit(current))
+    char symb = getchar();
+    if (symb == '\n')
     {
-        num->mantissa[len] = current - '0';
-        len++; 
+        printf("Пустой ввод\n");
+        return EMPTY_INPUT_ERROR;
     }
-    else
-        return NUM_READ_ERROR;
-    
-    current = getc(stdin);
-    while (isdigit(current))
+    if (symb != '+' && symb != '-')
     {
-        if (len > MANTISSA_LEN)
-            return NUM_READ_ERROR;
-        num->mantissa[len] = current - '0';
-        len++;
-        current = getc(stdin);
+        printf("Ошибка ввода: вначале не был введен знак\n");
+        return SIGN_ERROR;
+    }
+    a->sign = symb;
+
+    int i;
+    int addition_order = 0;
+    int flag_dot = 0;
+    int flag_digit = 0;
+
+    for (i = 0, symb = getchar(); (symb != '\n') && (symb != 'e') && (symb != 'E'); symb = getchar())
+    {
+        if (i >= MANTISSA_LEN)
+        {
+            printf("Произошло переполнение\n");
+            return OVERFLOW_ERROR;
+        }
+        if (symb == '.')
+        {
+            if (flag_dot == 0)
+                flag_dot = 1;
+            else
+            {
+                printf("Ошибка ввода: было введено более одной точки\n");
+                return INPUT_DOT_ERROR;
+            }
+        }
+        else
+        {
+            if ((symb >= '0') && (symb <= '9'))
+            {
+                if (flag_dot == 0)
+                {
+                    if ((symb > '0') || (i > 0))
+                    {
+                        a->mantissa[i] = atoi(&symb);
+                        flag_digit = 1;
+                        addition_order++;
+                        i++;
+                    }
+                }
+                else
+                {
+                    if ((symb > '0') || (flag_digit != 0))
+                    {
+                         a->mantissa[i] = atoi(&symb);
+                         flag_digit = 1;
+                         i++;
+                    }
+                    else
+                        addition_order--;
+                }
+            }
+            else
+            {
+                printf("Ошибка ввода: были введены не цифры\n");
+                return INPUT_DIGIT_ERROR;
+            }
+        }
     }
 
-    if (!isspace(current))
-        return NUM_READ_ERROR;
+    if ((symb == 'e') || (symb == 'E'))
+    {
+        if (scanf("%d", &(a->order)) != 1)
+        {
+            printf("Ошибка ввода: в порядке были введены не цифры\n");
+            return TYPE_ERROR;
+        }
+        if (a->order < MIN_ORDER)
+        {
+            printf("Порядок меньше критического значения\n");
+            return ORDER_ERROR;
+        }
+        else if (a->order > MAX_ORDER)
+        {
+            printf("Порядок больше критического значения\n");
+            return ORDER_ERROR;
+        }
+        getchar();
+    }
 
-    num->mantissa_len = len;
-
-    reverse_mantissa(num->mantissa, num->mantissa_len);
-    big_int_remove_leading_zeroes(num);
+    if (i == 0 && a->order != 0)
+    {
+        printf("Если хотите ввести ноль - вводите его c нулевым порядком\n");
+        return ZERO_ERROR;
+    }
+    a->order += addition_order;
 
     return EXIT_SUCCESS;
 }
 
-int big_float_input(big_float_t *num)
+int my_integer_input(integer_t *b, int *int_len)
 {
-    num->is_negative = 0;
-    num->exponent = 0;
-
-    size_t len = 0;
-    int num_shift = -1;
-
-    int dot_flag = 0;
-
-    char current = getc(stdin);
-
-    if (current == '-')
-        num->is_negative = 1;
-    else if (current == '+')
-        num->is_negative = 0;
-    else if (isdigit(current) && current != '0')
+    char symb = getchar();
+    if (symb == '\n')
     {
-        num->mantissa[len] = current - '0';
-        len++; 
+        printf("Пустой ввод");
+        return EMPTY_INPUT_ERROR;
     }
-    else if (current == '.')
+    if (symb != '+' && symb != '-')
     {
-        num_shift = 0;
-        dot_flag = 1;
+        printf("Ошибка ввода: вначале не был введен знак\n");
+        return SIGN_ERROR;
     }
-    else if (!isdigit(current))
-        return NUM_READ_ERROR;
-    
-    current = getc(stdin);
-    while (isdigit(current) || current == '.')
+    b->sign = symb;
+
+    int i;
+    int flag_zero = 1;
+    symb = getchar();
+
+    for (i = 0; symb != '\n'; symb = getchar())
     {
-        if (len > MANTISSA_LEN)
-            return NUM_READ_ERROR;
-        if (current == '.' && dot_flag == 1)
-            return NUM_READ_ERROR;
-        else if (current == '.' && dot_flag == 0)
+        if (i >= MANTISSA_LEN)
         {
-            num_shift = (int)len;
-            current = getc(stdin);
-            dot_flag = 1;
-            continue;
+            printf("Произошло переполнение");
+            return OVERFLOW_ERROR;
         }
-
-        num->mantissa[len] = current - '0';
-        len++;
-        current = getc(stdin);
-    }
-    if (!isspace(current))
-        return NUM_READ_ERROR;
-
-    num->mantissa_len = len;
-    reverse_mantissa(num->mantissa, num->mantissa_len);
-
-    if (num_shift == -1)
-        num_shift = len;
-
-    if (current == '\n')
-    {
-        if (num_shift != -1)
-            num->exponent += num_shift;
-        return EXIT_SUCCESS;
-    }
-
-    current = getc(stdin);
-    char exp[15];
-    if (current == 'E')
+        if (symb >= '0' && symb <= '9')
         {
-            if (fgets(exp, 15, stdin) == NULL || exp[strlen(exp) - 1] != '\n')
-                return NUM_READ_ERROR;
-            exp[strlen(exp) - 1] = '\0';
-            char *trash;
-            num->exponent = strtol(exp, &trash, 10); 
-            if (*trash != '\0')
-                return NUM_READ_ERROR;
-            
+            if (symb > '0' || flag_zero == 0)
+            {
+                b->digits[(*int_len)] = atoi(&symb);
+                (*int_len)++;
+                flag_zero = 0;
+                i++;
+            }
         }
-    else 
-        return NUM_READ_ERROR;
-
-    //if (num->exponent > 99999 || num->exponent < -99999)
-        //return EXPONENT_ERROR;
+        else
+        {
+            printf("Ошибка ввода: были введены не цифры\n");
+            return INPUT_DIGIT_ERROR;
+        }
+    }
+    if (flag_zero == 1)
+    {
+        printf("Деление на ноль невозможно\n");
+        return DIVISION_BY_ZERO;
+    }
     
-    if (num_shift != -1)
-        num->exponent += num_shift;
-
-    big_float_remove_leading_zeroes(num);
-
     return EXIT_SUCCESS;
 }
